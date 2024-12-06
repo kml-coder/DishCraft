2de44b794859e74986401e56d4376a8a96111aa9
lk4464 <kl946@scarletmail.rutgesr.edu>
2024-12-06 17:35:44 -0500

    changed

--- app/src/main/java/com/example/pizzaapp/MainActivity.java
+++ app/src/main/java/com/example/pizzaapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.pizzaapp.controller;
+package com.example.pizzaapp;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.example.pizzaapp.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pizzaapp.controller.OrderPizzaActivity;
